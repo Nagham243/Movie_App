6052139c27daa7db4baf359856ae4b79ae4402ca
Ahmed Mazen <ahmedmazenm@outlook.com>
2025-03-26 22:49:16 +0200

    applyed api key

--- src/apis/config.js
+++ src/apis/config.js
@@ -8,7 +8,8 @@ export const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   function (config) {
     // Do something before request is sent
-    config.headers["Authorization"] = "ACCESS_TOKEN_FROM_INTERC.";
+    config.params = config.params || {};
+    config.params["api_key"] = import.meta.env.VITE_API_KEY;
     return config;
   },
   function (error) {
